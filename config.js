1d3fd052620622a3455141e1c5298cd0fdc074f1
Karen Zhou <zz2202@nyu.edu>
2019-06-04 21:37:18 -0400

    s

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   stripe: {
-    apiKey: 'pk_test_SG9qh1GK5AiTycr7x4t3L9p6',
-    apiUrl: 'https://kpygs0yhak.execute-api.ap-southeast-1.amazonaws.com/dev/charges',
+    apiKey: 'pk_test_tBkVN9tMI061AG7I7nQ26V5N',
+    apiUrl: 'https://xh5afic3e1.execute-api.us-east-1.amazonaws.com/dev/charges',
     currency: 'USD',
   },
 };
