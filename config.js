753d86e9d36bb2769d39889ab28eb787462f92cd
Karen Zhou <zz2202@nyu.edu>
2019-06-04 21:55:28 -0400

    s

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   stripe: {
     apiKey: 'pk_test_tBkVN9tMI061AG7I7nQ26V5N',
-    apiUrl: 'https://xh5afic3e1.execute-api.us-east-1.amazonaws.com/dev/charges',
+    apiUrl: 'https://v6hr6b3vbi.execute-api.us-east-1.amazonaws.com/dev/charges',
     currency: 'USD',
   },
 };
